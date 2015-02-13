8676042912e0561bce291922f589a0a50c89d4c5
jkiyomu <jkiyomu@csula.edu>
2015-02-12 20:43:35 -0800

    implemented the images for the food and drinks.

--- js/controllers.js
+++ js/controllers.js
@@ -8,16 +8,16 @@ angular.module('resturant.robot').controller('ResturantController', function ($s
 	{'occupied':false,'disabled':false,'status':'empty'}
 	];
 
-
+	var debug = false;
 	$interval(doWorkSon,1000);
 
-	var promise = $interval(randomSim,5000);
+	var promise = $interval(randomSim,15000);
 
 	$scope.food = ["Pepperoni Pizza", "Fried Chicken", "Alfredo Pasta", "Triple Cheeseburger", "Ice Cream"];
 	$scope.drinks = ["Coca-Cola", "Water", "Tea", "Sprite", "Milkshake"];
 	$scope.orders = [];
 	$scope.message = 'Idle';
-	$scope.intervals = [{'name':'Busy','interval':500},{'name':'Moderate','interval':1000},{'name':'Slow','interval':5000}];
+	$scope.intervals = [{'name':'Busy','interval':5000},{'name':'Moderate','interval':10000},{'name':'Slow','interval':15000}];
 	$scope.currentInterval = 5000;
 
 	$scope.occupy = function(id) {
@@ -28,12 +28,14 @@ angular.module('resturant.robot').controller('ResturantController', function ($s
 	};
 
 	$scope.orderFood = function(id){
-		$scope.orders.push({'message':"Get " + $scope.food[Math.floor(Math.random()* $scope.food.length)] + " for table " + (id + 1),'id':id,type:'orderFood' });
+		var foodItem = $scope.food[Math.floor(Math.random()* $scope.food.length)];
+		$scope.orders.push({'message':"Give " + foodItem  + " for table " + (id + 1),'id':id,'type':'orderFood', 'give': 'true', 'item':foodItem });
 		$scope.tables[id].bill += Math.floor((Math.random() * 20) + 7);
 	}
 
 	$scope.orderDrink = function(id){
-		$scope.orders.push({'message':"Get " + $scope.drinks[Math.floor(Math.random()* $scope.drinks.length)] + " for table " + (id + 1),'id':id,type:'orderDrink' });
+		var foodItem = $scope.drinks[Math.floor(Math.random()* $scope.drinks.length)];
+		$scope.orders.push({'message':"Get " + foodItem + " for table " + (id + 1),'id':id,type:'orderDrink', give: 'true', item: foodItem  });
 		$scope.tables[id].bill += Math.floor((Math.random() * 3) + 1);
 	}
 
@@ -104,11 +106,32 @@ angular.module('resturant.robot').controller('ResturantController', function ($s
 					var order = $scope.orders.shift();
 					$scope.status.processing = true;
 					$scope.message = order.message;
-
+					
+					
+					if(order.type == 'orderFood' && order.give == 'true'){
+						jQuery('.robot').append("<img src='img/fullPlate.png' id='fullPlate' />");
+					}else if(order.type == 'orderDrink' && order.give == 'true'){
+						jQuery('.robot').append("<img src='img/fullCup.png' id='fullCup' />");
+					}
 					jQuery('.robot').animate({top:'400px',left:$('#table'+(order.id+1)).position().left},1500);
-					jQuery('.robot').animate({top:$('#table'+(order.id+1)).position().top,left:$('#table'+(order.id+1)).position().left},1500);
+					jQuery('.robot').animate({top:$('#table'+(order.id+1)).position().top,left:$('#table'+(order.id+1)).position().left},1500, function(){
+						if((order.type == 'orderFood' || order.type == 'orderDrink') && order.give == 'true'){
+							if(debug) alert('remove plate');
+							jQuery('img:last-child', this).remove();
+						}
+						if(order.type == 'orderFood' && order.give == 'false'){
+							jQuery('.robot').append("<img src='img/emptyPlate.png' id='emptyPlate' />");
+						}else if(order.type == 'orderDrink' && order.give == 'false'){
+							jQuery('.robot').append("<img src='img/emptyCup.png' id='emptyCup' />");
+						}
+					});
 					jQuery('.robot').animate({top:'400px',left:$('#table'+(order.id+1)).position().left},1500);
 					jQuery('.robot').animate({top:'400px',left:'250px'},1500,function(){
+						
+						if((order.type == 'orderFood' || order.type == "orderDrink") && order.give == 'false'){
+							jQuery('img:last-child', this).remove();
+						}
+						
 						$scope.status.processing = false;
 						if(order.type=='payCheck'){
 							$scope.tables[order.id].disabled = false;
@@ -116,6 +139,25 @@ angular.module('resturant.robot').controller('ResturantController', function ($s
 						}
 						$scope.message = 'Idle';
 					});
+					
+					
+					//This is to see once the food has been delivered then it will countdown a time for user to eat then to pick up empty plate.
+					if(order.type == "orderFood"){
+						if(debug) alert('orderFood '+order.item);
+						if(order.give == 'true'){
+							if(debug)alert('give true');
+							setTimeout(function(){
+								$scope.orders.push({'message':"Get " + order.item + " for table " + (order.id + 1),'id':order.id,type:'orderFood', give:'false', item:order.item  });
+							}, 3000);
+						}
+					}else if(order.type == "orderDrink"){
+						if(debug)alert('orderDrink '+order.item);
+						if(order.give == 'true'){
+							setTimeout(function(){
+								$scope.orders.push({'message':"Get " + order.item + " for table " + (order.id + 1),'id':order.id,type:'orderDrink', give:'false', item:order.item  });
+							}, 3000);
+						}
+					}
 				}
 				$scope.status.isWorking = false;
 			}
